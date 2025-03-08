f203a6ab4047ce29ca3ee2aa39926b6431e08f52
Nikhita305 <nikhitabhat033@gmail.com>
2025-03-08 11:47:13 +0530

    JaVA code

---
+++ Java.java
@@ -0,0 +1,9 @@
+class Java{
+public static void main(String[] args){
+System.out.println("Name: Nikhita");
+System.out.println("Blood Group: AB+");
+System.out.println("Gender: Female");
+System.out.println("Place: Uttara Kannada");
+System.out.println("DOB: November 4,2003");
+}
+}
